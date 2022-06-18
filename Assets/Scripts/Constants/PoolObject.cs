0fdc811622809f09565c9a7078bde30b751b3fd0
Andi-IM <andi.irhamm@gmail.com>
2022-06-18 23:07:00 +0700

    adding sesuatu yang dibutuhin

---
+++ Assets/Scripts/Constants/PoolObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Constants
+{
+    public enum PoolObjectType
+    {
+        Fireball, Waterball
+    }
+    
+    public class PoolObject : MonoBehaviour
+    {
+        public PoolObjectType type;
+        void Start()
+        {
+            Deactivate();
+        }
+
+        public void Activate(Vector3 position, Quaternion rotation)
+        {
+            gameObject.SetActive(true);
+            transform.position = position;
+            transform.rotation = rotation;
+        }
+
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+        }
+  
+        internal bool IsActive()
+        {
+            return gameObject.activeInHierarchy;
+        }
+    }
+}
